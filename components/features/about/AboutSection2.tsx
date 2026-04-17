8d95ba103cd3629f03c9c29ba3056949ce4c6bfd
dyedai <4250ikiad@gmail.com>
2026-04-17 17:23:25 +0900

    section2まで

--- components/features/about/AboutSection2.tsx
+++ components/features/about/AboutSection2.tsx
@@ -17,8 +17,7 @@ export default function AboutSection2() {
     <section className="relative overflow-hidden min-h-screen flex items-center justify-center gap-12 lg:gap-50 px-8">
       <FloatingCloud src="/kumo3.svg" direction="right" width={540} height={150} className="top-10 left-240" />
       <FloatingCloud src="/kumo4.svg" direction="left"  width={540} height={150} className="bottom-[5px] left-0 zindex-20" />
-      <FloatingCloud src="/kumo6.svg" direction="right" width={540}  height={150} className="bottom-32 right-0" />
-
+      <FloatingCloud src="/kumo6.svg" direction="right" width={540}  height={150*0.9} className="bottom-[0px] right-0" />
       <SectionTitle title="私について" />
       <VerticalColumns columns={COLUMNS} className="leading-[50px] text-2xl tracking-[0.2em]" />
     </section>
